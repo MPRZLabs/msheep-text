1f2644856240e53127053aec5ce86c08a9b1c575
michcioperz <michcioperz@gmail.com>
2013-02-22 12:35:32 +0100

    Engineering...

---
+++ src/mprz/textline/TheGame.java
@@ -0,0 +1,151 @@
+package mprz.textline;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.fusesource.jansi.Ansi.Color;
+
+/**
+ *
+ * @author michcioperz <michcioperz@gmail.com
+ */
+public class TheGame {
+    mSheep sheep;
+    LE7ELS le7els;
+    
+    public TheGame() {
+        sheep = mSheep.getInstance();
+        le7els = new LE7ELS();
+        sheep.currentLoc = le7els.start;
+    }
+    
+    public static void main(String[] args) {
+        try {
+            TheGame game = new TheGame();
+            game.sheep.run();
+        } catch (InterruptedException | IOException ex) {
+            Logger.getLogger(TheGame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    public class LE7ELS {
+        
+        Location start;
+        
+        public LE7ELS() {
+            start = new Location() {
+                
+                GameObject[] map;
+
+                @Override
+                public String getName() {
+                    return "Chocolate Factory";
+                }
+
+                @Override
+                public GameObject[] getObjectsList() {
+                    return map;
+                }
+
+                @Override
+                public void onArrival() {
+                    map = new GameObject[1];
+                    map[0] = new GameObject() {
+                        
+                        boolean produced = false;
+
+                        @Override
+                        public String getName() {
+                            return "Chocolate producing machine";
+                        }
+
+                        @Override
+                        public void onUse() {
+                            if (produced) {
+                                sheep.out.println("There's no more cacao in the machine.");
+                                sheep.out.flush();
+                                try {
+                                    Thread.sleep(500);
+                                } catch (InterruptedException ex) {
+                                    Logger.getLogger(February1GAM.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                                sheep.out.println(sheep.color("WHY IS THIS WORLD SO CRUEL?!", Color.YELLOW) + " you shout out loud, but not anyone and not anything seems to have heard it.");
+                                sheep.out.flush();
+                            } else {
+                                sheep.out.println("Awesome! You make a chocolate bar using remains of cacao resting in the machine!");
+                                sheep.out.flush();
+                                produced = true;
+                                sheep.inventory[0] = new GameObject() {
+
+                                    @Override
+                                    public String getName() {
+                                        return "Chocolate bar";
+                                    }
+
+                                    @Override
+                                    public void onUse() {
+                                        sheep.inventory[0] = null;
+                                        sheep.effects[0] = null;
+                                        sheep.out.println("Hunger leaves you as you eat a medium quality chocolate bar.");
+                                        sheep.out.println("Did you really think a chocolate bar made from remains of cacao resting in the machine will be as delicious as a chocolate bar made from fresh chocolate?");
+                                        sheep.out.flush();
+                                        try {
+                                            Thread.sleep(2000);
+                                        } catch (InterruptedException ex) {
+                                            Logger.getLogger(February1GAM.class.getName()).log(Level.SEVERE, null, ex);
+                                        }
+                                        sheep.out.println("Okay, you didn't have time to think. That's sure a good reason. Now, why don't you get out of here?");
+                                        sheep.out.flush();
+                                    }
+
+                                    @Override
+                                    public GameObject[] getParent() {
+                                        return sheep.inventory;
+                                    }
+
+                                    @Override
+                                    public String getCodename() {
+                                        return "chocobar";
+                                    }
+                                };
+                            }
+                        }
+
+                        @Override
+                        public GameObject[] getParent() {
+                            return start.getObjectsList();
+                        }
+
+                        @Override
+                        public String getCodename() {
+                            return "machine";
+                        }
+                    };
+                    sheep.out.println("You wake up in the chocolate factory.");
+                    sheep.out.println("All alone, with no one nearby.");
+                    sheep.out.println("On own survival.");
+                    sheep.out.flush();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(February1GAM.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    sheep.out.println("Also, you feel a bit hungry.");
+                    sheep.effects[0] = "You feel hungry.";
+                    sheep.out.flush();
+                }
+
+                @Override
+                public void onLeave() {
+                    
+                }
+
+                @Override
+                public void onLookover() {
+                    sheep.out.println("Only thing you can see is a chocolate machine nearby.");
+                }
+            };
+        }
+    }
+}
--- src/mprz/textline/mSheep.java
+++ src/mprz/textline/mSheep.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mprz.textline;
 
 import java.io.IOException;
@@ -9,6 +5,7 @@ import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jline.console.ConsoleReader;
+import jline.console.completer.AggregateCompleter;
 import jline.console.completer.StringsCompleter;
 import org.fusesource.jansi.Ansi.Color;
 
@@ -18,11 +15,12 @@ import org.fusesource.jansi.Ansi.Color;
  */
 public class mSheep {
     
-    private ConsoleReader console;
-    private PrintWriter out;
-    private GameObject[] inventory;
-    private String[] effects;
-    private StringsCompleter strCompleter = new StringsCompleter("help", "inventory", "stop", "sheep", "effects", "lookover", "use");
+    public ConsoleReader console;
+    public PrintWriter out;
+    public GameObject[] inventory;
+    public String[] effects;
+    public StringsCompleter strCompleter = new StringsCompleter("help", "inventory", "stop", "sheep", "effects", "lookover", "use");
+    public Location currentLoc;
     
     public String color(String text, int color) {
         return "\u001B[" + color + "m" + text + "\u001B[" + Color.WHITE.fg() + "m";
@@ -76,6 +74,119 @@ public class mSheep {
         out.println();
         out.flush();
         Thread.sleep(1000);
+    }
+    
+    public void stop() {
+        out.println();
+        out.println();
+        out.println(color("MPRZ Tech Labs", Color.CYAN));
+        out.println(color("Transmission done.", Color.RED));
+        out.flush();
+    }
+    
+    public void sheep() throws InterruptedException, InterruptedException {
+        out.println("mSHEEP Text Game Engine");
+        out.println();
+        out.println("           /\\0");
+        out.println("          /" + color("_", Color.GREEN) + " \\0");
+        out.println("         /  " + color("_", Color.GREEN) + " \\@@@@@@@@@   @");
+        out.println("        /\\    @#########@ @#@");
+        out.println("        \\ \\/ @###########@###@");
+        out.println("         \\  @#############@#@");
+        out.println("          \\@###############@");
+        out.println("          @###############@");
+        out.println("          @###############@");
+        out.println("           @#############@");
+        out.println("            @###########@");
+        out.println("             @#########@");
+        out.println("              @@@@@@@@@");
+        out.println("              /|      |\\");
+        out.println("             / |      | \\");
+        out.println("            /---      ---\\");
+        out.println();
+        out.println();
+        out.flush();
+        Thread.sleep(1000);
+    }
+    
+    public void run() throws InterruptedException, IOException {
+        currentLoc.onArrival();
+        String line;
+        while (true) {
+            line = console.readLine(color("mprz: ", Color.GREEN));
+            if (line.startsWith("stop")) {
+                stop();
+                break;
+            } else if (line.startsWith("help")) {
+                out.println(color("help", Color.MAGENTA) + "           shows help");
+                out.println(color("stop", Color.MAGENTA) + "           exits game");
+                out.println(color("inventory", Color.MAGENTA) + "      lists your character's inventory");
+                out.println(color("effects", Color.MAGENTA) + "        lists physical and psychical effects affecting your character");
+                out.println(color("sheep", Color.MAGENTA) + "          uses " + color("the Mysterious Magic of Green Unicorns from Parallel Universe Where Edison Never Existed", Color.CYAN) + " to draw a black hole in time-space continuum fabric");
+                out.println(color("lookover", Color.MAGENTA) + "       asks your character's eyes for things it sees");
+                out.println(color("use", Color.MAGENTA) + "            lets you use an object from the inventory or the environment");
+                out.flush();
+            } else if (line.startsWith("inventory")) {
+                for (GameObject iter: inventory) {
+                    if (iter != null) {
+                        out.println(color(iter.getCodename(), Color.MAGENTA) + " " + iter.getName());
+                        out.flush();
+                        Thread.sleep(250);
+                    }
+                }
+            } else if (line.startsWith("effects")) {
+                for (String iter: effects) {
+                    if (iter != null) {
+                        out.println(iter);
+                        out.flush();
+                        Thread.sleep(250);
+                    }
+                }
+            } else if (line.startsWith("sheep")) {
+                sheep();
+            } else if (line.startsWith("use")) {
+                AggregateCompleter agComp = new AggregateCompleter();
+                for (GameObject iter: inventory) {
+                    if (iter != null) {
+                        agComp.getCompleters().add(new StringsCompleter(iter.getCodename()));
+                    }
+                }
+                for (GameObject iter: currentLoc.getObjectsList()) {
+                    if (iter != null) {
+                        agComp.getCompleters().add(new StringsCompleter(iter.getCodename()));
+                    }
+                }
+                console.removeCompleter(strCompleter);
+                console.addCompleter(agComp);
+                String useLine = console.readLine(color("mprz:use: ", Color.GREEN));
+                boolean objUsed = false;
+                for (GameObject iter: inventory) {
+                    if (iter != null && !objUsed) {
+                        if (useLine.startsWith(iter.getCodename())) {
+                            iter.onUse();
+                            objUsed = true;
+                        }
+                    }
+                }
+                for (GameObject iter: currentLoc.getObjectsList()) {
+                    if (iter != null && !objUsed) {
+                        if (useLine.startsWith(iter.getCodename())) {
+                            iter.onUse();
+                            objUsed = true;
+                        }
+                    }
+                }
+                if (!objUsed) {
+                    out.println("Can't use something that's not on the list!");
+                    out.flush();
+                }
+                console.removeCompleter(agComp);
+                console.addCompleter(strCompleter);
+            } else if (line.startsWith("lookover")) {
+                currentLoc.onLookover();
+            }
+            Thread.sleep(500);
+        }
     }
     
     private static class mSheepHolder {
